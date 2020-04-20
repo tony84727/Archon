eb034e91e9290b76ec69cdbabb6c0cc2058466d6
tony84727 <tony84727@gmail.com>
2020-04-21 00:36:09 +0800

    remove example mod

---
+++ src/main/kotlin/com/github/tony84727/archon/Archon.kt
@@ -0,0 +1,12 @@
+package com.github.tony84727.archon
+
+import net.minecraftforge.fml.common.Mod
+
+@Mod(modid = Archon.MODID, version = Archon.VERSION)
+class Archon {
+    companion object {
+        const val MODID = "archone"
+        const val VERSION = "0.0.0"
+    }
+
+}
